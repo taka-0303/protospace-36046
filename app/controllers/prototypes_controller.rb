8347e71cbe645aa6cc1d92ada41a04b10a7c1a50
taka-0303 <87518654+taka-0303@users.noreply.github.com>
2021-09-27 22:31:30 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,9 +1,10 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.includes(:user)
    end
  
    def new
@@ -20,17 +21,14 @@ class PrototypesController < ApplicationController
    end
  
    def show
-     @prototype = Prototype.find(params[:id])
      @comment = Comment.new
      @comments = @prototype.comments
    end
  
    def edit
-     @prototype = Prototype.find(params[:id])
    end
  
    def update
-     @prototype = Prototype.find(params[:id])
      @prototype.update(prototype_params)
      if @prototype.save
        redirect_to prototype_path
@@ -51,9 +49,11 @@ class PrototypesController < ApplicationController
      params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
    end
 
+   def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
    def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+    redirect_to root_path unless current_user == @prototype.user
   end
 end
